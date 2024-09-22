0b95954c2781ddbc46a1b349a1eeb821816a63b3
Boolsh <pekshin05@mail.ru>
2024-09-22 22:50:31 +0300

    Добавьте файлы проекта.

---
+++ шарпы 2 лаба/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace шарпы_2_лаба
+{
+    internal class Program
+{    static double taylor(double x, double epsilon)
+    {
+        double term = 1/2.0 * x; 
+        double sum = 1 + term; 
+        int n = 0; 
+
+        while (Math.Abs(term) > epsilon)
+        {
+            term *= -x * ((2 * n + 1.0) / (2 * n + 4.0) );
+            sum += term;
+            n++;
+        }
+
+        return sum;
+    }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите X0 (|X0| < 1):");
+            double X0 = Convert.ToDouble(Console.ReadLine());
+            //double X0 = -0.1;
+
+
+            Console.WriteLine("Введите Xn (|Xn| < 1):");
+            double Xn = Convert.ToDouble(Console.ReadLine());
+            //double Xn = 0.1;
+
+
+            Console.WriteLine("Введите шаг h:");
+            double h = Convert.ToDouble(Console.ReadLine());
+           // double h = 0.1;
+
+
+            Console.WriteLine("Введите точность E:");
+            double epsilon = Convert.ToDouble(Console.ReadLine());
+            //double epsilon = 0.00001;
+
+
+
+            Console.WriteLine("\n----------------------------------------------------");
+            Console.WriteLine("|   X   |   f(X) вычисл. (Тейлор)   |   f(X) по формуле   |");
+            Console.WriteLine("----------------------------------------------------");
+
+            for (double x = X0; x <= Xn; x += h)
+            {
+                double taylorApprox = taylor(x, epsilon);
+                double exactValue = Math.Sqrt(1 + x);
+
+                Console.WriteLine($"| {x,5:F2} | {taylorApprox,25:F15} | {exactValue,19:F15} |");
+            }
+
+            Console.WriteLine("----------------------------------------------------");
+        }
+    }
+}
---
+++ шарпы 2 лаба/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("шарпы 2 лаба")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("шарпы 2 лаба")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("707b0e90-cc4f-4c23-9788-5784d60bc2d3")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
